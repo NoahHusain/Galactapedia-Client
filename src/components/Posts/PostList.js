8b42943582a9ddea87bf232e6bca2aedb5211c20
Noah Husain <noahhusain@gmail.com>
2021-09-15 11:30:58 -0500

    Finished login and register page. added basic rendering of stellar objects on main page and some basic css.

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -1,104 +1,49 @@
-// import { PostContext } from "./PostProvider";
-// import React, { useContext, useEffect } from "react";
-// import { useHistory, Link } from "react-router-dom";
-// import "./Post.css";
+import { PostContext } from "./PostProvider";
+import React, { useContext, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import "./Post.css";
 
-// export const PostList = () => {
-//   const { getPosts, posts, deletePost } = useContext(PostContext);
-//   const history = useHistory();
+export const PostList = () => {
+  const { getStellarObjects, stellarObjects } = useContext(PostContext);
+  const history = useHistory();
 
-//   useEffect(() => {
-//     getPosts();
-//   }, []);
+  useEffect(() => {
+    getStellarObjects();
+  }, []);
 
-//   const handleDeletePost = (postId) => {
-//     deletePost(postId);
+//   const handleDeletePost = (wikiId) => {
+//     deletePost(wikiId);
 //   };
 
-//   const handleUpdatePost = (postId) => {
-//     console.log(postId);
-//     history.push(`/posts/edit/${postId}`);
+//   const handleUpdatePost = (wikiId) => {
+//     console.log(wikiId);
+//     history.push(`/posts/edit/${wikiId}`);
 //   };
 
-//   const deletePostButton = (userId, postId) => {
-//     if (userId == localStorage.getItem("mines_customer") || localStorage.getItem("mines_customer") === "5") {
-//       return <button
-//       className="post blueText"
-//       id={`post--${postId}`}
-//       onClick={(event) => {
-//         event.preventDefault();
-//         handleDeletePost(postId);
-//       }}
-//     >
-//       Delete Post
-//     </button>
-//     }
-//     else {
-//       return 
-//     }
-//   }
-  
-//   const editPostButton = (userId, postId) => {
-//     if (userId == localStorage.getItem("mines_customer") || localStorage.getItem("mines_customer") === "5" ) {
-//       return <button
-//       className="post blueText"
-//       id={`post--${postId}`}
-//       onClick={(event) => {
-//         event.preventDefault();
-//         handleUpdatePost(postId);
-//       }}
-//     >
-//       Edit Post
-//     </button>
-//     }
-//     else {
-//       return 
-//     }
-//   }
+  return (
+    <>
+      <h1 className="goldenRodText center">Recent Articles</h1>
 
-//   return (
-//     <>
-//       <h1 className="goldenRodText center">Posts</h1>
+      <button
+        className="create__button"
+        onClick={() => history.push("/Posts/create")}
+      >
+        Create New Wiki
+      </button>
 
-//       <button
-//         className="create__button"
-//         onClick={() => history.push("/Posts/create")}
-//       >
-//         Found an Ore Patch?
-//       </button>
-
-//       {posts.map((post) => (
-//         <section className="posts">
-//           <article className="post" id={`post--${post.id}`}>
-//             <div className="post__oreId blueText">Ore: {post.ore.name}</div>
-//             <div className="post__planetId blueText">
-//               Planet: {post.planet.name}
-//             </div>
-//             <div className="post__moonId blueText">
-//               {post.moon ? `Moon: ${post.moon.name}` : null}
-//               {/* If moon has an ID, render the div. If not, do not render the div */}
-//             </div>
-//             <div className="post__landingPoint blueText">
-//               Landing Point: {post.landingPoint}
-//             </div>
-//             <div className="post__description blueText">
-//               Description: {post.description}
-//             </div>
-//             <div className="post_rockData blueText">
-//               Largest Rock Found: {post.rockData}
-//             </div>
-//             <div className="post__userId blueText">
-//               Posted By: {post.user.name} on {post.timestamp}
-//             </div>
-//             {deletePostButton(post.userId, post.id)}
-//             {editPostButton(post.userId, post.id)}
+      {stellarObjects.map((stellarObject) => (
+        <section className="posts">
+          <article className="post" id={`stellarObject--${stellarObject.id}`}>
+            <div className="blueText"><a >{stellarObject.name}</a></div>
+            <div className="blueText">Mass: {stellarObject.mass}</div>
+            <div className="blueText">Radius: {stellarObject.radius}</div>
             
-//           </article>
-//         </section>
-//       ))}
-//     </>
-//   );
-// };
+          </article>
+        </section>
+      ))}
+    </>
+  );
+};
 
 
 
--- src/components/Posts/PostProvider.js
+++ src/components/Posts/PostProvider.js
@@ -1,70 +1,42 @@
-// import React, { useState, createContext } from "react";
+import React, { useState, createContext } from "react";
 
-// // The context is imported and used by individual components that need data
-// export const PostContext = createContext();
+// The context is imported and used by individual components that need data
+export const PostContext = createContext();
 
-// // This component establishes what data can be used.
-// export const PostProvider = (props) => {
-//   const [posts, setPosts] = useState([]);
+// This component establishes what data can be used.
+export const PostProvider = (props) => {
+  const [stellarObjects, setStellarObjects] = useState([]);
 
-//   const getPosts = () => {
-//     return fetch(
-//       "https://mines-r-us-api-44tok.ondigitalocean.app/orePatches?_expand=user&_expand=ore&_expand=planet&_expand=moon"
-//     )
-//       .then((res) => res.json())
-//       .then(setPosts);
-//   };
+  const api = "http://localhost:8000"
 
-//   const addPost = (postObject) => {
-//     return fetch("https://mines-r-us-api-44tok.ondigitalocean.app/orePatches", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(postObject),
-//     }).then(getPosts);
-//   };
+  const getStellarObjects = () => {
+    return fetch(`${api}/stellarobjects`, {
+    headers: {
+        "Authorization": `Token ${localStorage.getItem("Galactapedia_user_token")}`
+    }})
+      .then((res) => res.json())
+      .then(setStellarObjects);
+  };
 
-//   const deletePost = (orePatchId) => {
-//     return fetch(
-//       `https://mines-r-us-api-44tok.ondigitalocean.app/orePatches/${orePatchId}`,
-//       {
-//         method: "DELETE",
-//       }
-//     ).then(getPosts);
-//   };
+  const addStellarObject = (stellarObject) => {
+    return fetch(`${api}/stellarobjects`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(stellarObject),
+    }).then(getStellarObjects);
+  };
 
-//   const updatePost = (orePatch) => {
-//     return fetch(
-//       `https://mines-r-us-api-44tok.ondigitalocean.app/orePatches/${orePatch.id}`,
-//       {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(orePatch),
-//       }
-//     ).then(getPosts);
-//   };
-
-//   const getPostById = (orePatchId) => {
-//     return fetch(
-//       `https://mines-r-us-api-44tok.ondigitalocean.app/orePatches/${orePatchId}`
-//     ).then((res) => res.json());
-//   };
-
-//   return (
-//     <PostContext.Provider
-//       value={{
-//         posts,
-//         getPosts,
-//         addPost,
-//         deletePost,
-//         updatePost,
-//         getPostById,
-//       }}
-//     >
-//       {props.children}
-//     </PostContext.Provider>
-//   );
-// };
+  return (
+    <PostContext.Provider
+      value={{
+        stellarObjects,
+        getStellarObjects,
+        addStellarObject
+      }}
+    >
+      {props.children}
+    </PostContext.Provider>
+  );
+};
